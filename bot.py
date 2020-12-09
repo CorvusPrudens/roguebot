f4981aa1d5d6af8d6a9ce08a4b0429152f36fd3c
CorvusPrudens <corvusprudens@gmail.com>
2020-12-08 19:03:34 -0500

    running env

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 # import asyncio
 import json
+import re
+import sys
 
 class Bot:
     def __init__(self, savepath):
-    self.savepath = savepath
-        pass
+        self.savepath = savepath
+        self.funcDict = {
+            '.coins': self.fCoins,
+            '.map': self.fMap,
+        }
+        commRegexString = '('
+        for key in self.funcDict:
+            commRegexString += '(\\{})|'.format(key)
+        self.commRegex = re.compile(commRegexString[:-1] + ')\\b')
+        self.coins = 0
+        self.run = True
 
     def load():
-        try: 
+        try:
             with open(self.savepath, 'r') as file:
                 self.palyers = json.load(file)
         except KeyError:
@@ -17,5 +28,17 @@ class Bot:
         with open(self.savepath, 'w') as file:
             json.dump(self.palyers, file)
 
-    def loop(self):
+    def loop(self, message, enter):
+        if enter:
+            command = self.commRegex.search(message)
+            if command != None:
+                self.funcDict[command.group(0)](message)
         pass
+
+    def fCoins(self, message):
+        string = 'You have {} coins\n'.format(self.coins)
+        sys.stdout.write(string)
+
+    def fMap(self, message):
+        string = '.....\n'
+        sys.stdout.write(string)
--- exec.py
+++ exec.py
@@ -1,47 +1,46 @@
 import bot
 import time
 import kb as KB
+import sys
 
 ppath = 'saves/players.json'
 roguebot = bot.Bot(ppath)
+kb = KB.KBHit()
 
-
-while (roguebot.run):
-	roguebot.loop()
-	time.sleep(1)
-
- kb = KB.KBHit()
-
-print('Hit any key, or ESC to exit')
+print('Hit ESC to exit')
 chrbuf = ''
 mess = ''
 enter = False
 prevtime = time.process_time()
 
-while roguebot.run::
+while roguebot.run:
 
     if kb.kbhit():
         c = kb.getch()
         if ord(c) == 27: # ESC
             break
      # print(c, end='')
-        if ord(c) == 127:
+        if ord(c) == 127 or ord(c) == 8:
             if len(chrbuf) > 0:
                 chrbuf = chrbuf[:-1]
                 sys.stdout.write('\b \b')
-        elif ord(c) == 10:
+        elif ord(c) == 10 or ord(c) == 13:
             enter = True
             mess = chrbuf
             chrbuf = ''
+            sys.stdout.write('\n')
         else:
             chrbuf += c
-            sys.stdout.write(c + str(ord(c)))
+            # sys.stdout.write(c + str(ord(c)))
+            sys.stdout.write(c)
         sys.stdout.flush()
 
-    if time.process_time() - prevtime >= 1:
+    if time.process_time() - prevtime >= 0.5:
         prevtime = time.process_time()
         roguebot.loop(mess, enter)
+        if enter:
+            enter = False
 
-    
+
 
 kb.set_normal_term()
